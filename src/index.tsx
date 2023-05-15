af0c4a1440071d342d9174c73e1b330b5e442928
ABubon <bubonandr@yandex.ru>
2023-05-16 02:42:41 +0300

    sprint-17 refactoring #1.2

--- src/index.tsx
+++ src/index.tsx
@@ -48,7 +48,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/react-burger">
         <App />
       </BrowserRouter>
     </Provider>
